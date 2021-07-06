4441dc753d5c1a81254af3eb5b572c1e1591618a
2018119148 <2514263568@qq.com>
2021-07-06 10:13:05 +0800

    实验2

---
+++ 实验2/shiyan2/targets/STM32L431_BearPi/GCC/appbuild/los_event.d
@@ -0,0 +1,194 @@
+appbuild/los_event.d appbuild/los_event.o: \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/base/ipc/los_event.c \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/base/include/los_priqueue.ph \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_list.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_typedef.h \
+ D:/shiyan/shiyan2/targets/STM32L431_BearPi/OS_CONFIG/los_builddef.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_compiler.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_typedef.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/base/include/los_task.ph \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_task.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_base.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_config.h \
+ D:/shiyan/shiyan2/targets/STM32L431_BearPi/OS_CONFIG/target_config.h \
+ D:/shiyan/shiyan2/targets/STM32L431_BearPi/Inc/stm32l4xx.h \
+ D:/shiyan/shiyan2/targets/STM32L431_BearPi/Inc/stm32l431xx.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/arch/arm/common/cmsis/core_cm4.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/arch/arm/common/cmsis/cmsis_compiler.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/arch/arm/common/cmsis/cmsis_gcc.h \
+ D:/shiyan/shiyan2/targets/STM32L431_BearPi/Inc/system_stm32l4xx.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ D:/shiyan/shiyan2/targets/STM32L431_BearPi/Inc/stm32l4xx_hal_conf.h \
+ D:/shiyan/shiyan2/targets/STM32L431_BearPi/Inc/main.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_base.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_config.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_sys.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/arch/arm/arm-m/include/los_hwi.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/base/include/los_event.ph \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_event.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_list.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_adc.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc_ex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_qspi.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rng.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi_ex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h \
+ D:/shiyan/shiyan2/targets/STM32L431_BearPi/OS_CONFIG/los_printf.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_errno.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_err.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_compiler.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_sys.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_tick.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_event.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/arch/arm/arm-m/include/los_hw.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/base/ipc/los_event.inc
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/base/include/los_priqueue.ph:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_list.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_typedef.h:
+
+D:/shiyan/shiyan2/targets/STM32L431_BearPi/OS_CONFIG/los_builddef.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_compiler.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_typedef.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/base/include/los_task.ph:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_task.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_base.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_config.h:
+
+D:/shiyan/shiyan2/targets/STM32L431_BearPi/OS_CONFIG/target_config.h:
+
+D:/shiyan/shiyan2/targets/STM32L431_BearPi/Inc/stm32l4xx.h:
+
+D:/shiyan/shiyan2/targets/STM32L431_BearPi/Inc/stm32l431xx.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/arch/arm/common/cmsis/core_cm4.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/arch/arm/common/cmsis/cmsis_compiler.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/arch/arm/common/cmsis/cmsis_gcc.h:
+
+D:/shiyan/shiyan2/targets/STM32L431_BearPi/Inc/system_stm32l4xx.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+
+D:/shiyan/shiyan2/targets/STM32L431_BearPi/Inc/stm32l4xx_hal_conf.h:
+
+D:/shiyan/shiyan2/targets/STM32L431_BearPi/Inc/main.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_base.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_config.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_sys.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/arch/arm/arm-m/include/los_hwi.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/base/include/los_event.ph:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_event.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_list.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_adc.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc_ex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_qspi.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rng.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi_ex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
+
+D:/shiyan/shiyan2/targets/STM32L431_BearPi/OS_CONFIG/los_printf.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_errno.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_err.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_compiler.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_sys.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_tick.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/include/los_event.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/arch/arm/arm-m/include/los_hw.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/liteos/base/ipc/los_event.inc:
