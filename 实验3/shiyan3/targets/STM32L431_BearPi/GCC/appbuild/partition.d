1ff15b2587744f07865ac744b0d2e086b5a2c027
2018119148 <2514263568@qq.com>
2021-07-06 10:15:38 +0800

    Signed-off-by: 2018119148 <“2514263568@qq.com”>

---
+++ 实验3/shiyan3/targets/STM32L431_BearPi/GCC/appbuild/partition.d
@@ -0,0 +1,8 @@
+appbuild/partition.d appbuild/partition.o: \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/storage/partition.c \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/storage/partition.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/storage/storage.h
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/storage/partition.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/storage/storage.h:
---
+++ 实验3/shiyan3/targets/STM32L431_BearPi/GCC/appbuild/queue.d
@@ -0,0 +1,14 @@
+appbuild/queue.d appbuild/queue.o: \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/queue/queue.c \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/osal/osal.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/link_misc/link_misc.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/osal/osal_types.h \
+ C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/queue/queue.h
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/osal/osal.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/link_misc/link_misc.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/osal/osal_types.h:
+
+C:/Users/李瑞强/.icode/sdk/IoT_LINK_1.0.0/iot_link/queue/queue.h:
